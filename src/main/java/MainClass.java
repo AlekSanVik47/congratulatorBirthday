39e0db5427ab1f579a0bc7c57e821e9f0eabfa75
qa_al <qa_aleksvik@outlook.com>
2022-01-27 11:42:30 +0400

    добавил

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -60,6 +60,7 @@ public class MainClass {
 
 //        writing.inputFileToMap(listBirth, Path.of("fileMap")); // запись объекта в файл (Map)
 //        writing.outputFile(Path.of("fileMap")); // чтение объекта из файла
+
     }
 
 }
